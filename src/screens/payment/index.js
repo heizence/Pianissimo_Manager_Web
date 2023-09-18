f818d420e546c6109eed108c24952861f2536ef0
Doheon Keum <47074893+heizence@users.noreply.github.com>
2023-09-18 17:36:13 +0900

    Update index.js

--- src/screens/payment/index.js
+++ src/screens/payment/index.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import {
+  KAKAOPAY_API_KEY,
+  KAKAOPAY_APPROVAL_URL,
+  KAKAOPAY_CANCEL_URL,
+  KAKAOPAY_FAIL_URL,
+} from "../../modules/secretKeys";
 import NavigationTab from "../../modules/NavigationTab";
 import FindStudentModal from "./FindStudentModal";
 import "./index.css";
@@ -37,7 +43,7 @@ const PaymentMain = () => {
         method: "POST",
         headers: {
           // 카카오 developers에 등록한 admin키를 헤더에 줘야 한다.
-          Authorization: "KakaoAK 6b15e93c3130f6c2fa45a9fd260184dc",
+          Authorization: KAKAOPAY_API_KEY,
           "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
         },
         // 설정한 매개변수들
@@ -50,9 +56,9 @@ const PaymentMain = () => {
           total_amount: 100000 * TicketMonth, // 상품 총액
           tax_free_amount: 0, // 상품 비과세 금액
 
-          approval_url: "http://localhost:3000/kakaopayRedirect/success", // 결제 성공 시 redirect url
-          cancel_url: "http://localhost:3000/kakaopayRedirect/cancel", // 결제 취소 시 redirect url
-          fail_url: "http://localhost:3000/kakaopayRedirect/fail", // 결제 실패 시 redirect url
+          approval_url: `http://${KAKAOPAY_APPROVAL_URL}/kakaopayRedirect/success`, // 결제 성공 시 redirect url
+          cancel_url: `http://${KAKAOPAY_CANCEL_URL}/kakaopayRedirect/cancel`, // 결제 취소 시 redirect url
+          fail_url: `http://${KAKAOPAY_FAIL_URL}/kakaopayRedirect/fail`, // 결제 실패 시 redirect url
         },
       }).then(
         (response) => {
@@ -85,12 +91,7 @@ const PaymentMain = () => {
             <span className="PaymentMain_inputLabel">원생 이름</span>
 
             <div className="PaymentMain_inputSection">
-              <input
-                disabled={true}
-                id="PaymentMain_input"
-                type="text"
-                value={StudentName}
-              />
+              <input disabled={true} id="PaymentMain_input" type="text" value={StudentName} />
               <button
                 className="PaymentMain_eachBtn"
                 onClick={() => {
@@ -107,31 +108,14 @@ const PaymentMain = () => {
             <span className="PaymentMain_inputLabel">이용권</span>
 
             {/* 연습실 이용권 checkbox */}
-            <div
-              className="PaymentMain_inputSection"
-              id="PaymentMain_checkboxDiv"
-            >
-              <div
-                className="PaymentMain_eachCheckbox"
-                onClick={() => SetTicketType("연습실")}
-              >
-                <input
-                  type="checkbox"
-                  checked={TicketType === "연습실"}
-                  readOnly
-                />
+            <div className="PaymentMain_inputSection" id="PaymentMain_checkboxDiv">
+              <div className="PaymentMain_eachCheckbox" onClick={() => SetTicketType("연습실")}>
+                <input type="checkbox" checked={TicketType === "연습실"} readOnly />
                 <span>연습실 이용권</span>
               </div>
               {/* 레슨 이용권 checkbox */}
-              <div
-                className="PaymentMain_eachCheckbox"
-                onClick={() => SetTicketType("레슨")}
-              >
-                <input
-                  type="checkbox"
-                  checked={TicketType === "레슨"}
-                  readOnly
-                />
+              <div className="PaymentMain_eachCheckbox" onClick={() => SetTicketType("레슨")}>
+                <input type="checkbox" checked={TicketType === "레슨"} readOnly />
                 <span>레슨 이용권</span>
               </div>
             </div>
@@ -140,18 +124,12 @@ const PaymentMain = () => {
           <div className="PaymentMain_eachInputSection">
             <span className="PaymentMain_inputLabel">이용 개월 수</span>
             <div className="PaymentMain_inputSection">
-              <select
-                id="PaymentMain_dropdownBtn"
-                name="months"
-                onChange={(e) => SetTicketMonth(e.target.value)}
-              >
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(
-                  (number, index) => (
-                    <option key={index} value={number}>
-                      {number}개월
-                    </option>
-                  )
-                )}
+              <select id="PaymentMain_dropdownBtn" name="months" onChange={(e) => SetTicketMonth(e.target.value)}>
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((number, index) => (
+                  <option key={index} value={number}>
+                    {number}개월
+                  </option>
+                ))}
               </select>
             </div>
           </div>
